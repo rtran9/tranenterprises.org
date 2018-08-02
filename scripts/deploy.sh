c58b604bffb5a59ed63c6d6f0e6ae7b7d377b22a
robert <rtran@mit.edu>
2018-08-02 14:44:15 -0400

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,8 @@
-aws s3 sync sites/www.tranenterprises.org/public s3://www.tranenterprises.org/ --delete
+aws s3 sync sites/www.tranenterprises.org/public s3://www.tranenterprises.org --delete --content-type text/html
 
 aws s3 sync sites/www.robert.tranenterprises.org/public/assets s3://www.robert.tranenterprises.org/assets --delete
-aws s3 sync sites/www.robert.tranenterprises.org/public s3://www.robert.tranenterprises.org/ --delete --content-type text/html --exclude 'assets/*'
+aws s3 sync sites/www.robert.tranenterprises.org/public s3://www.robert.tranenterprises.org --delete --content-type text/html --exclude 'assets/*'
+
+aws s3 sync sites/www.anne.tranenterprises.org/public s3://www.anne.tranenterprises.org --delete --content-type text/html
+aws s3 sync sites/www.patrick.tranenterprises.org/public s3://www.patrick.tranenterprises.org --delete --content-type text/html
+aws s3 sync sites/www.philip.tranenterprises.org/public s3://www.philip.tranenterprises.org --delete --content-type text/html
