85f0bea4e6d53d2a1c5e8236bc177e06de9c7d5b
robert <rtran@mit.edu>
2019-01-21 16:36:41 -0500

    test with disabled keys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
-aws s3 sync sites/www.tranenterprises.org/public s3://www.tranenterprises.org --delete --content-type text/html --debug
+aws s3 sync sites/www.tranenterprises.org/public s3://www.tranenterprises.org --delete --content-type text/html
 
-aws s3 sync sites/www.robert.tranenterprises.org/public/assets s3://www.robert.tranenterprises.org/assets --delete --debug
-aws s3 sync sites/www.robert.tranenterprises.org/public s3://www.robert.tranenterprises.org --delete --content-type text/html --exclude 'assets/*' --debug
+aws s3 sync sites/www.robert.tranenterprises.org/public/assets s3://www.robert.tranenterprises.org/assets --delete
+aws s3 sync sites/www.robert.tranenterprises.org/public s3://www.robert.tranenterprises.org --delete --content-type text/html --exclude 'assets/*'
 
-aws s3 sync sites/www.anne.tranenterprises.org/public s3://www.anne.tranenterprises.org --delete --content-type text/html --debug
-aws s3 sync sites/www.patrick.tranenterprises.org/public s3://www.patrick.tranenterprises.org --delete --content-type text/html --debug
-aws s3 sync sites/www.philip.tranenterprises.org/public s3://www.philip.tranenterprises.org --delete --content-type text/html --debug
+aws s3 sync sites/www.anne.tranenterprises.org/public s3://www.anne.tranenterprises.org --delete --content-type text/html
+aws s3 sync sites/www.patrick.tranenterprises.org/public s3://www.patrick.tranenterprises.org --delete --content-type text/html
+aws s3 sync sites/www.philip.tranenterprises.org/public s3://www.philip.tranenterprises.org --delete --content-type text/html
